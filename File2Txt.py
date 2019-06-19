6bdf00d6d0c639b65dfb6755d835e3c88936999c
slick <y0slick@protonmail.com>
2019-06-18 21:37:54 -0500

    Added files via upload
    Added File2Txt, Log Calc (py, pyui, and uipack), Pymgur, Script_Downloader (py, pyui, uipack.)

---
+++ File2Txt.py
@@ -0,0 +1,15 @@
+import console, os, sys
+
+if __name__ == '__main__':
+    console.clear()
+    filename = raw_input('Enter filename (Path Optional) \n')
+    if not filename:
+        sys.exit('No filename entered.')
+    if filename.endswith('.txt'):
+        sys.exit('Can not convert .txt to .txt.')
+    try:
+        with open(filename, 'r') as in_file:
+            with open(os.path.splitext(filename)[0] + '.txt', 'w') as out_file:
+                out_file.write(in_file.read())
+    except IOError:
+        console.hud_alert('File not found: ' + filename, icon = 'error')
